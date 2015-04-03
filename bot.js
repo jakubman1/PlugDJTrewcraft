dc9303a8ff1b85771013c4054235000f06c9862f
jakubman1 <jakubman1@gmail.com>
2015-04-03 13:25:27 +0200

    bot.js

---
+++ bot.js
@@ -0,0 +1,117 @@
+(function () {
+    //Link location of your fork so you don't have to modify so many things.
+    var fork = "bot";
+		
+    //Define our function responsible for extending the bot.
+    function extend() {
+        //If the bot hasn't been loaded properly, try again in 1 second(s).
+        if (!window.bot) {
+            return setTimeout(extend, 1 * 1000);
+        }
+
+        //Precaution to make sure it is assigned properly.
+        var bot = window.bot;
+
+        //Load custom settings set below
+        bot.retrieveSettings();
+
+        /*
+         Extend the bot here, either by calling another function or here directly.
+         Model code for a bot command:
+         bot.commands.commandCommand = {
+         command: 'cmd',
+         rank: 'user/bouncer/mod/manager',
+         type: 'startsWith/exact',
+         functionality: function(chat, cmd){
+         if(this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+         if( !bot.commands.executable(this.rank, chat) ) return void (0);
+         else{
+         //Commands functionality goes here.
+         }
+         }
+         }
+         */
+
+        bot.commands.mcCommand = {
+            command: 'mc',  //The command to be called. With the standard command literal this would be: !bacon
+            rank: 'user', //Minimum user permission to use the command
+            type: 'exact', //Specify if it can accept variables or not (if so, these have to be handled yourself through the chat.message
+            functionality: function (chat, cmd) {
+                if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                if (!bot.commands.executable(this.rank, chat)) return void (0);
+                else {
+                    API.sendChat("IP serveru je server.trewcraft.eu");
+					API.sendChat("Web serveru je http://trewcraft.eu");
+                }
+            }
+        };
+
+        //Load the chat package again to account for any changes
+        bot.loadChat();
+
+    }
+
+    //Change the bots default settings and make sure they are loaded on launch
+
+    localStorage.setItem("basicBotsettings", JSON.stringify({
+        botName: "TCBot",
+        language: "czech",
+        startupCap: 1, // 1-200
+        startupVolume: 0, // 0-100
+        startupEmoji: true, // true or false
+        chatLink: "https://plug.fleximedia.cz/" + fork + "/cs.json",
+        maximumAfk: 120,
+        afkRemoval: false,
+        maximumDc: 300,
+        bouncerPlus: true,
+        blacklistEnabled: true,
+        lockdownEnabled: false,
+        lockGuard: false,
+        maximumLocktime: 10,
+        cycleGuard: true,
+        maximumCycletime: 10,
+        voteSkip: true,
+        voteSkipLimit: 3,
+        timeGuard: true,
+        maximumSongLength: 7,
+        autodisable: true,
+        commandCooldown: 30,
+        usercommandsEnabled: true,
+        lockskipPosition: 3,
+        lockskipReasons: [
+            ["theme", "Toto video se nehodí do této komunity. "],
+            ["op", "Toto video je na seznamu OP videí. "],
+            ["history", "Toto video je v historii.  "],
+            ["mix", "Hrát mixy není povoleno. "],
+            ["sound", "Video nemá zvuk nebo je velmi nekvalitní. "],
+            ["nsfw", "Je zakázáno hrát nevhodná videa "],
+            ["unavailable", "Video, které jste se pokusil přehrát je pro některé uživatele nedostupné. "]
+        ],
+        afkpositionCheck: 15,
+        afkRankCheck: "ambassador",
+        motdEnabled: false,
+        motdInterval: 5,
+        motd: "Temporary Message of the Day",
+        filterChat: true,
+        etaRestriction: false,
+        welcome: true,
+        opLink: null,
+        rulesLink: null,
+        themeLink: null,
+        fbLink: null,
+        youtubeLink: null,
+        website: null,
+        intervalMessages: [],
+        messageInterval: 5,
+        songstats: true,
+        commandLiteral: "!",
+        blacklists: {
+            NSFW: "https://rawgit.com/" + fork + "/basicBot-customization/master/blacklists/ExampleNSFWlist.json",
+            OP: "https://rawgit.com/" + fork + "/basicBot-customization/master/blacklists/ExampleOPlist.json"
+        }
+    }));
+
+    //Start the bot and extend it when it has loaded.
+    $.getScript("https://rawgit.com/Yemasthui/basicBot/master/basicBot.js", extend);
+
+}).call(this);
